ec43e05d6bc20d2e88f1bfca6d7ff56e088df540
CodeBy-Ryan <ryanrodrigues9890@gmail,com>
2025-01-12 10:57:17 -0300

    Primeiro commit: Adicionando arquivos iniciais

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+// Scroll suave para âncoras
+document.querySelectorAll("a[href^='#']").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    // Garantir que o alvo exista
+    if (targetElement) {
+      e.preventDefault();
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+        block: "start", // Ajuste para centralizar melhor
+      });
+    }
+  });
+});
+
+// Menu hambúrguer
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
